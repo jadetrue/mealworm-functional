4bb4bd9c0210b0f7c7930247974616a13d09f1ac
Shea Murphy <Shea.Murphy@nology.io>
2020-08-17 11:04:27 +0100

    Converted App to functional

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,18 @@ import CardFront from "./components/CardFront";
 import CardBack from "./components/CardBack";
 import NavBar from "./components/NavBar";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <section className={styles.nav}>
-          <NavBar />
-        </section>
-        <section className={styles.content}>
-          <CardFront />
-          <CardBack />
-        </section>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <section className={styles.nav}>
+        <NavBar />
+      </section>
+      <section className={styles.content}>
+        <CardFront />
+        <CardBack />
+      </section>
+    </>
+  );
+};
 
 export default App;
