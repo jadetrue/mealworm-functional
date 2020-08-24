0070d9d7d6c79bb05f172c154a943fffaa9ee629
Shea Murphy <Shea.Murphy@nology.io>
2020-08-24 14:17:42 +0100

    Updated feedback panel in dashboard

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./Dashboard.module.scss";
-import CardList from '../../components/CardList';
-import FeedbackPanel from '../../components/FeedbackPanel';
+import CardList from "../../components/CardList";
+import FeedbackPanel from "../../components/FeedbackPanel";
 
 const DashBoard = (props) => {
-  const {recipes} = props;
+  const { recipes } = props;
 
   const toggleFav = (recipe) => {
     recipe.isFav = !recipe.isFav;
@@ -14,12 +14,12 @@ const DashBoard = (props) => {
     <CardList recipes={recipes} toggleFav={toggleFav} />
   ) : (
     <FeedbackPanel
-      header="No matches"
-      text="None of our recipes matched that search"
+      header="No recipes"
+      text="Type into the searchbar to find your desired meal"
     />
   );
-  
-  return <section className={styles.dashboard}>{contentJsx}</section>
+
+  return <section className={styles.dashboard}>{contentJsx}</section>;
 };
 
 export default DashBoard;
