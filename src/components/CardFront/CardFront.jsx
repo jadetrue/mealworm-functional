882735d0a75b14f886f35d4920f0465852d58547
andynology <70571501+andynology@users.noreply.github.com>
2020-10-05 11:58:40 +0100

    fix to code

--- src/components/CardFront/CardFront.jsx
+++ src/components/CardFront/CardFront.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import styles from "./CardFront.module.scss";
+import React from "react";
 
-export default CardFront = () => {
+const CardFront = () => {
   return (
-    <div>
-      <div className={styles.cardFront}>
-        <img src="https://www.themealdb.com/images/media/meals/sstssx1487349585.jpg" alt="Thai Green Curry" />
-        <section className={styles.recipeDetails}>
-          <h2>Thai Green Curry</h2>
-          <p>
-            <span>Thai</span>
-          </p>
-          <div className={styles.links}>
-            <a href="" target="_blank" rel="noopener noreferrer">
-              Full recipe
-            </a>
-            <a href="https://www.youtube.com/watch?v=LIbKVpBQKJI" target="_blank" rel="noopener noreferrer">
-              Youtube
-            </a>
-          </div>
-        </section>
-      </div>
+    <div className={styles.cardFront}>
+      <img
+        src="https://www.themealdb.com/images/media/meals/sstssx1487349585.jpg"
+        alt="Thai Green Curry"
+      />
+      <section className={styles.recipeDetails}>
+        <h2>Thai Green Curry</h2>
+        <p>
+          <span>Thai</span>
+        </p>
+        <div className={styles.links}>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            Full recipe
+          </a>
+          <a
+            href="https://www.youtube.com/watch?v=LIbKVpBQKJI"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Youtube
+          </a>
+        </div>
+      </section>
     </div>
   )
-}
+};
+
+export default CardFront;
