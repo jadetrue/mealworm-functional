64c19b2b621070a565dbe847091cd6be521a5639
Jade True <jadetruexo@gmail.com>
2021-11-10 12:09:10 +0000

    Added styles to my component and rendered the component on the App

--- src/components/CardFront/CardFront.jsx
+++ src/components/CardFront/CardFront.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import styles from "./CardFront.module.scss";
 
 export const CardFront = () => {
-    return <div>Card Front Component</div>;
+    return (
+        <div className={styles.cardFront}>
+            <img src="thai-green-curry.jpg" alt="Thai Green Curry" />
+            <section className={styles.recipeDetails}>
+                <h2>Thai Green Curry</h2>
+                <p>
+                    <span>Thai</span>
+                </p>
+                <div className={styles.links}>
+                    <a href="">Full recipe</a>
+                    <a href="">Youtube</a>
+                </div>
+            </section>
+        </div>
+    );
 };
