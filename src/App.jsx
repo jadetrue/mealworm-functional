8effdbe2090eb6f0b738e421f77b248663fcf54c
Jade True <jadetruexo@gmail.com>
2021-11-16 20:43:36 +0000

    Completed challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import styles from "./App.module.scss";
 import CardFront from "./components/CardFront";
 import CardBack from "./components/CardBack";
@@ -7,21 +7,23 @@ import NavBar from "./components/NavBar";
 import recipes from "./data/recipes";
 
 const App = () => {
-  const getCardFrontJsx = (recipe) => (
-    <CardFront key={recipe.idMeal} recipe={recipe} />
-  );
+    const alternate = (recipe) => (
+        <>
+            <CardFront key={recipe.idMeal} recipe={recipe} />
+            <CardBack recipe={recipe} />
+        </>
+    );
 
-  return (
-    <>
-      <section className={styles.nav}>
-        <NavBar />
-      </section>
-      <section className={styles.content}>
-        {recipes.map(getCardFrontJsx)}
-        <CardBack recipe={recipes[1]} />
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className={styles.nav}>
+                <NavBar />
+            </section>
+            <section className={styles.content}>
+                {recipes.map(alternate)}
+            </section>
+        </>
+    );
 };
 
 export default App;
