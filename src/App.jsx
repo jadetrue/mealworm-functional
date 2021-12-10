dce29fe44a8b4765c7156198b0b5605c316101f7
Jade True <jadetruexo@gmail.com>
2021-12-10 11:58:28 +0000

    Container files and new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import styles from "./App.module.scss";
+
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
+import NotFound from "./components/NotFound/index";
 
-import library from "./data/fa-library";
 import recipes from "./data/recipes";
 
-const App = () => {
-  const getCardJsx = (recipe) => (
-    <div className={styles.card} key={recipe.idMeal}>
-      <Card recipe={recipe} />
-    </div>
-  );
+import Dashboard from "./containers/Dashboard/Dashboard";
+import Routes from "./containers/Routes/Routes";
+// import Cookbook from "./containers/Cookbook/Cookbook";
 
-  return (
-    <>
-      <section className={styles.nav}>
-        <NavBar />
-      </section>
-      <section className={styles.content}>{recipes.map(getCardJsx)}</section>
-    </>
-  );
+const App = () => {
+    const getCardJsx = (recipe) => (
+        <div className={styles.card} key={recipe.idMeal}>
+            <Card recipe={recipe} />
+        </div>
+    );
+
+    return (
+        <>
+            <section className={styles.nav}>
+                <NavBar />
+            </section>
+            <section className={styles.content}>
+                {recipes.map(getCardJsx)}
+            </section>
+            {/* <Cookbook /> */}
+            <NotFound />
+            <Dashboard />
+            <Routes />
+        </>
+    );
 };
 
 export default App;
---
+++ src/containers/Cookbook/Cookbook.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cookbook = () => {
+    return <div>Cookbook</div>;
+};
+
+export default Cookbook;
